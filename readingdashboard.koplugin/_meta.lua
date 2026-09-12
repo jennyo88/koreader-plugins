4f0fca817c10799e1d83d78c5afc5bb2776b0fda
jennyo88 <39907844+jennyo88@users.noreply.github.com>
2026-09-12 10:05:13 -0700

    Bump version from 0.1.0 to 0.2.0
    v0.2.0 - Add reading statistics

--- readingdashboard.koplugin/_meta.lua
+++ readingdashboard.koplugin/_meta.lua
@@ -4,5 +4,5 @@ return {
     name = "readingdashboard",
     fullname = _("Reading Dashboard"),
     description = _([[A compact reading dashboard for KOReader showing progress and basic information about the current book.]]),
-    version = "0.1.0",
+    version = "0.2.0",
 }
